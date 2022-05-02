a7b6c7901dd5a8539af6ae9cd5f4ba37671780ed
santhanakrishna.offical@gmail.com <Santhana9>
2022-05-02 11:02:29 +0530

    Added signing in, updating sessions, fencing off protected routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    before_action :ensure_user_logged_in
+    def ensure_user_logged_in
+        unless current_user
+            redirect_to "/"
+        end
+    end 
+    
+    def current_user
+        return @current_user if @current_user #memoization the value the 
+        # user stored only once during a session in which the action called multiple times
+        # The instance variable is available only during a single request-response cycle once the request is processed the value is reset
+
+        current_user_id = session[:current_user_id]
+        if current_user_id
+            @current_user = User.find(current_user_id)
+        else
+            nil
+        end
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+    skip_before_action :ensure_user_logged_in
     def index
+        if current_user
+            redirect_to todos_path
+        else
+        #@current_user_id = session[:current_user_id]
         render "index"
+        end 
     end
 end
